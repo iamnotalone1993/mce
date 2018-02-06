b0c991b9f69e0c0b582a5fad07fc9d371c58c88a
kldz1412 <minhtringuyen1412@gmail.com>
2018-02-06 18:28:46 +0700

    add checkConflict func

--- detector.c
+++ detector.c
@@ -28,6 +28,196 @@ int toInt(char a[]) {
   return n;
 }
 
+char *toTokenArray(char *str){
+	char *  p    = strtok (str, "\t");
+	int size = 0;
+	char ** res  = NULL;
+	while (p) {
+	res = realloc (res, sizeof (char*) * ++size);
+
+	if (res == NULL) {
+		printf("Memory allocation failed.Terminating..\n");
+		exit (-1); /* memory allocation failed */
+	}
+
+	res[size-1] = p;
+
+	p = strtok (NULL, " ");
+	}
+	return res;
+};
+
+int *getClock(char *str){
+	char * p = strtok(str, "|");
+	int *res, size = 0;
+	while( p ) {
+	 	res = realloc (res, sizeof (int) * ++size);
+
+		if (res == NULL) {
+			printf("Memory allocation failed.Terminating..\n");
+			exit (-1); /* memory allocation failed */
+		}
+
+		res[size-1] = toInt(p);
+    	p = strtok(NULL, "|");
+	}
+	return res;
+};
+
+void checkConflict(char *op1,int rank1, char *op2, int rank2, char **shared, bool isInside){
+	char **op1_tokens = toTokenArray(op1);
+	char **op2_tokens = toTokenArray(op2);
+	size_t size;
+	size = sizeof(op1_tokens) / sizeof(op1_tokens[0]);
+	int *clock1 = getClock(op1_tokens[size-1]);
+	size = sizeof(op2_tokens) / sizeof(op1_tokens[0]);
+	int *clock2 = getClock(op2_tokens[size-1]);
+	//RMA: op 	origin_addr		origin_count	target_rank		target_disp		target_count	clock
+	//LS: op 	address		clock
+	if (strstr(op1,"Put")) {
+		//if 2 ops are not in the same process, then op1 is in origin and op2 is in target
+		if (strstr(op2,"Store")) {
+			if (isInside){
+				if (strcmp(op1_tokens[1],op2_tokens[1]) == 0 && clock1[rank1] <= clock2[rank2]) { //compare address and clock
+					printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+				}
+			}
+			else {
+				//array are not considered
+				if (toInt(op1_tokens[3]) == rank2) {
+					printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+				}
+			}
+			
+		}
+		else if (strstr(op2,"Load")){
+			if (!isInside && toInt(op1_tokens[3]) == rank2) {
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else if (strstr(op2,"Put")) {
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //rank == rank && disp == disp -> same address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else if (strstr(op2,"Get")) {
+			if (isInside && strcmp(op1_tokens[1], op2_tokens[1]) == 0) {
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //put to a, get from a (a = address)
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else { //Accumulate
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //rank == rank && disp == disp -> same address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		return;
+
+	}
+
+	if (strstr(op1,"Get")){
+		if (strstr(op2,"Store")) {
+			if (isInside){
+				if (strcmp(op1_tokens[1],op2_tokens[1]) == 0 && clock1[rank1] <= clock2[rank2]) { //compare address and clock
+					printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+				}
+			}
+			else {
+				//array are not considered
+				if (toInt(op1_tokens[3]) == rank2) {
+					printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+				}
+			}
+			
+		}
+		else if (strstr(op2,"Load")){
+			if (isInside && strcmp(op1_tokens[1],op2_tokens[1]) == 0) { //inside process and same address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else if (strstr(op2,"Put")) {
+			if (isInside && strcmp(op1_tokens[1], op2_tokens[1]) == 0) { //get/put same origin address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //rank == rank && disp == disp -> same address, get/put from/to same address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else if (strstr(op2,"Get")) {
+			if (isInside && strcmp(op1_tokens[1], op2_tokens[1]) == 0) { //get/put same origin address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else { //Accumulate
+			if (isInside && strcmp(op1_tokens[1], op2_tokens[1]) == 0) { //get/put same origin address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //rank == rank && disp == disp -> same address, get/put from/to same address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		return;
+	}
+
+	if (strstr(op1,"Accumulate")){
+		if (strstr(op2,"Store")) {
+			if (isInside){
+				if (strcmp(op1_tokens[1],op2_tokens[1]) == 0 && clock1[rank1] <= clock2[rank2]) { //compare address and clock
+					printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+				}
+			}
+			else {
+				//array are not considered
+				if (toInt(op1_tokens[3]) == rank2) {
+					printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+				}
+			}
+			
+		}
+		else if (strstr(op2,"Load")){
+			if (!isInside && toInt(op1_tokens[3]) == rank2) {
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else if (strstr(op2,"Put")) {
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //rank == rank && disp == disp -> same address
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else if (strstr(op2,"Get")) {
+			if (isInside && strcmp(op1_tokens[1], op2_tokens[1]) == 0) {
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+			if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //put to a, get from a (a = address)
+				printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			}
+		}
+		else { //Accumulate
+			//if (strcmp(op1_tokens[3], op2_tokens[3]) == 0 && strcmp(op1_tokens[4], op2_tokens[4]) == 0) { //rank == rank && disp == disp -> same address
+				//printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+			//}
+		}
+		return;
+	}
+
+	if (strstr(op1,"Load")){
+		if ((strstr(op2,"Put") || strstr(op2,"Accumulate")) && !isInside && toInt(op2_tokens[3]) == rank1) {	
+			printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+		}
+		return;
+	}
+
+	if (strstr(op1,"Store")){
+		if ((strstr(op2,"Put") || strstr(op2,"Accumulate") || strstr(op2,"Get")) && !isInside && toInt(op2_tokens[3]) == rank1) {
+			printf("These ops are conflict:\n1: %s\n2: %s\n", op1, op2);
+		}
+		return;
+	}
+};
+
+
 int main( int argc, char *argv[] )  {
 	int numfile;
 	int i;
@@ -91,24 +281,9 @@ int main( int argc, char *argv[] )  {
 							if (strstr(lines[i],"Put")) {
 								if (strstr(tmpline,"Store")) {
 									//generate 2 arrays of token
-									char **put_arr = 0;
-									put_arr = malloc (sizeof (char*) * 7);
-									char *  p    = strtok (lines[i], "\t");
-									int i = 0;
-									while (p) {
-										put_arr[i] = p;
-										i++;
-										p = strtok (NULL, "\t");
-									}
-									char **store_arr = 0;
-									store_arr = malloc (sizeof (char*) * 3);
-									p = strtok (tmpline, "\t");
-									i = 0;
-									while (p) {
-										store_arr[i] = p;
-										i++;
-										p = strtok (NULL, "\t");
-									}
+									char **put_arr = toTokenArray(lines[i]);
+									
+									char **store_arr = toTokenArray(tmpline);
 									if (strcmp(put_arr[1],store_arr[1]) == 0) { //same address
 										//report
 									}
