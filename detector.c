4328af3bc2966d6d353fc09feaff219e55ae0038
Thanh-Dang Diep <iamnotalone1993@gmail.com>
2018-02-19 10:03:27 +0100

    Updating all files

--- detector.c
+++ detector.c
@@ -1,5 +1,63 @@
 #include "detector.h"
 
+List insertCommNode(List aList, int code, char *origin_addr, int target_rank, char *target_addr/*, int *clock, int size*/)
+{
+	if (aList.commHead == NULL && aList.commTail == NULL)
+	{
+		aList.commTail = (Comm *) malloc(sizeof(Comm));
+		aList.commHead = aList.commTail;
+	}
+	else
+	{
+		aList.commTail->next = (Comm *) malloc(sizeof(Comm));
+		aList.commTail = aList.commTail->next;
+	}
+	aList.commTail->code = code;
+	aList.commTail->origin_addr = origin_addr;
+	aList.commTail->target_rank = target_rank;
+	aList.commTail->target_addr = target_addr;
+	//aList.commTail->clock = (int *) malloc (size * sizeof(int));
+	aList.commTail->next = NULL;
+	return aList;	
+}
+
+List insertLocaNode(List aList, int code, char *varAddr)
+{
+	if (aList.locaHead == NULL && aList.locaTail == NULL)
+        {
+                aList.locaTail = (Loca *) malloc(sizeof(Loca));
+                aList.locaHead = aList.locaTail;
+        }
+        else
+        {
+                aList.locaTail->next = (Loca *) malloc(sizeof(Loca));
+                aList.locaTail = aList.locaTail->next;
+        }
+	aList.locaTail->code = code;
+	aList.locaTail->varAddr = varAddr;
+	aList.locaTail->next = NULL;
+	return aList;
+}
+
+Chai insertChainNode(Chai aChain, int code, char *varAddr)
+{
+	if (aChain.head == NULL && aChain.tail == NULL)
+	{
+		aChain.tail = (Loca *) malloc(sizeof(Loca));
+		aChain.head = aChain.tail;
+	}
+	else
+	{
+		aChain.tail->next = (Loca *) malloc(sizeof(Loca));
+		aChain.tail = aChain.tail->next;
+	}
+	aChain.tail->code = code;
+	aChain.tail->varAddr = (char *) malloc((strlen(varAddr) + 1) * sizeof (char));
+	strcpy(aChain.tail->varAddr, varAddr);
+	aChain.tail->next = NULL;
+	return aChain;
+}
+
 int getEventCode(char *str)
 {
 	if (strstr(str, "Fence") != NULL)
@@ -36,6 +94,78 @@ int getEventCode(char *str)
 		return CREATE;
 }
 
+char *convertCode2Name(int code)
+{
+	char *tmp;
+	switch (code)
+	{
+		case FENCE:
+			tmp = malloc(6 * sizeof(char));
+			strcpy(tmp, "FENCE\0");
+			return tmp;
+		case BARRIER:
+                        tmp = malloc(8 * sizeof(char));
+                        strcpy(tmp, "BARRIER\0");
+                        return tmp;
+		case GET:
+                        tmp = malloc(4 * sizeof(char));
+                        strcpy(tmp, "GET\0");
+                        return tmp;
+		case PUT:
+                        tmp = malloc(4 * sizeof(char));
+                        strcpy(tmp, "PUT\0");
+                        return tmp;
+		case ACCUMULATE:
+                        tmp = malloc(11 * sizeof(char));
+                        strcpy(tmp, "ACCUMULATE\0");
+                        return tmp;
+        	case POST:
+                        tmp = malloc(5 * sizeof(char));
+                        strcpy(tmp, "POST\0");
+                        return tmp;
+        	case START:
+                        tmp = malloc(6 * sizeof(char));
+                        strcpy(tmp, "START\0");
+                        return tmp;
+		case COMPLETE:
+                        tmp = malloc(9 * sizeof(char));
+                        strcpy(tmp, "COMPLETE\0");
+                        return tmp;
+		case WAIT:
+                        tmp = malloc(5 * sizeof(char));
+                        strcpy(tmp, "WAIT\0");
+                        return tmp;
+        	case LOCK:
+                        tmp = malloc(5 * sizeof(char));
+                        strcpy(tmp, "LOCK\0");
+                        return tmp;
+        	case UNLOCK:
+                        tmp = malloc(7 * sizeof(char));
+                        strcpy(tmp, "UNLOCK\0");
+                        return tmp;
+		case SEND:
+                        tmp = malloc(5 * sizeof(char));
+                        strcpy(tmp, "SEND\0");
+                        return tmp;
+		case RECV:
+                        tmp = malloc(5 * sizeof(char));
+                        strcpy(tmp, "RECV\0");
+                        return tmp;
+        	case LOAD:
+                        tmp = malloc(5 * sizeof(char));
+                        strcpy(tmp, "LOAD\0");
+                        return tmp;
+        	case STORE:
+                        tmp = malloc(6 * sizeof(char));
+                        strcpy(tmp, "STORE\0");
+                        return tmp;
+        	case CREATE:
+                        tmp = malloc(7 * sizeof(char));
+                        strcpy(tmp, "CREATE\0");
+                        return tmp;
+        }
+}
+
 int *getClock(char *str)
 {
 	int *clock, size, i;
@@ -61,17 +191,99 @@ bool equalClock(int *clock1, int *clock2, int size)
 	return true;
 }
 
+char *getData(char **buffer)
+{
+	char *tmpBuffer, *tmpStr;
+	int tmpInt;
+	
+	tmpBuffer = *buffer;
+	*buffer = strchr(*buffer, '\t') + 1;
+        tmpInt = strcspn(tmpBuffer, "\t");
+        tmpStr = (char *) malloc((tmpInt + 1) * sizeof(char));
+        memcpy(tmpStr, tmpBuffer, tmpInt);
+	tmpStr[tmpInt] = '\0';
+
+	return tmpStr;
+}
+
+void detectMCEInProc(Chai aChain)
+{
+	if (aChain.head == NULL && aChain.tail == NULL) 
+	{ 
+		/* do nothing */ 
+	}
+	else 
+	{
+		Loca *tmp;
+		while (aChain.head != NULL)
+		{
+			aChain.tail = aChain.head;
+			if (aChain.head->code != LOAD && aChain.head->code != STORE)
+                        {
+				tmp = aChain.head->next;
+                        	while (tmp != NULL)
+                        	{
+                                	if (aChain.head->code == GET)
+                                	{
+						if (strcmp(aChain.head->varAddr, tmp->varAddr) == 0)
+						{
+                                                	printf("MCE within an epoch on %s between %s and %s\n", tmp->varAddr, 
+                                                        	convertCode2Name(aChain.head->code), convertCode2Name(tmp->code));
+						}
+						else //if (strcmp(aChain.tail->varAddr, tmp->varAddr) != 0)
+						{
+							/* do nothing */
+						}
+                                	}
+                                	else if (aChain.head->code == PUT || aChain.head->code == ACCUMULATE)
+                                	{
+						if (tmp->code == GET || tmp->code == STORE)
+						{
+							if (strcmp(aChain.head->varAddr, tmp->varAddr) == 0)
+                                                	{
+                                                        	printf("MCE within an epoch on %s between %s and %s\n", tmp->varAddr, 
+									convertCode2Name(aChain.head->code), convertCode2Name(tmp->code));
+                                                	}
+                                                	else //if (strcmp(aChain.tail->varAddr, tmp->varAddr) != 0)
+                                                	{
+                                                        	/* do nothing */
+                                                	}
+						}
+						else //if (tmp->varAddr->code != GET && tmp->varAddr->code != STORE)
+						{
+							/* do nothing */
+						}
+                                	}
+                                	else //if (aChain.tail->code == LOAD || aChain.tail->code == STORE) 
+                                	{
+                                        	/* do nothing */
+                                	}
+                                	tmp = tmp->next;
+                        	}
+                        }
+                        else //if (aChain.tail->code == LOAD || aChain.tail->code == STORE) 
+                        {
+                                        /* do nothing */
+                        }
+			aChain.head = aChain.head->next;
+			free(aChain.tail->varAddr);
+			aChain.tail->next = NULL;
+                	free(aChain.tail);
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
-	int size, i, index, *clock, *tmpClock, tmpInt, eventCode;
+	int size, i, index, *clock, *tmpClock, tmpInt, eventCode, j;
 	char fileName[15];
 	char *buffer, *clockStr, *tmpBuffer, *tmpStr;
 	FILE **pFile;
-	List *head;
+	List *aList;
 
 	size = atoi(argv[1]);
 	pFile = (FILE **) malloc(size * sizeof(FILE *));
-	head = (List *) malloc(size * sizeof(List));
+	aList = (List *) malloc(size * sizeof(List));
 	for (i = 0; i < size; i++)
 	{
 		sprintf(fileName, "trace%d", i);
@@ -83,21 +295,23 @@ int main(int argc, char **argv)
 
 		buffer = (char *) malloc(BUFFER_SIZE * sizeof(char));
 		fgets(buffer, BUFFER_SIZE, pFile[i]);
-		tmpBuffer = strchr(buffer, '\t') + 1;
-		memcpy(head[i].base, tmpBuffer, 10);
-		tmpBuffer = strchr(tmpBuffer, '\t') + 1;
-		tmpInt = strcspn(tmpBuffer, "\t");
-		tmpStr = (char *) malloc(tmpInt * sizeof(char));
-		memcpy(tmpStr, tmpBuffer, tmpInt);
-		head[i].size = atoi(tmpStr);
-		printf("%d\n", head[i].size);
+
+		tmpBuffer = buffer;
+                tmpStr = getData(&tmpBuffer);
+		aList[i].base = tmpStr;
+
+		tmpStr = getData(&tmpBuffer);
+		aList[i].size = atoi(tmpStr);
 		free(tmpStr);
-		tmpBuffer = strchr(tmpBuffer, '\t') + 1;
-		tmpInt = strcspn(tmpBuffer, "\t");
-		tmpStr = (char *) malloc(tmpInt * sizeof(char));
-		memcpy(tmpStr, tmpBuffer, tmpInt);
-		head[i].disp_unit = atoi(tmpStr);
+
+		tmpStr = getData(&tmpBuffer);
+		aList[i].disp_unit = atoi(tmpStr);
 		free(tmpStr);
+
+		aList[i].commHead = NULL;
+		aList[i].commTail = NULL;
+		aList[i].locaHead = NULL;
+		aList[i].locaTail = NULL;
 		free(buffer);
 	}
 
@@ -106,7 +320,9 @@ int main(int argc, char **argv)
 	while (index < size)
 	{
 		if (fgets(buffer, BUFFER_SIZE, pFile[index]) != NULL)
-		{	
+		{              
+			printf("C1: %s", buffer);
+                        getchar();
 			if (getEventCode(buffer) == FENCE)
 			{
 				clockStr = (char *) malloc((size * 2 + 2) * sizeof(char));
@@ -120,6 +336,8 @@ int main(int argc, char **argv)
 					{
 						if (fgets(buffer, BUFFER_SIZE, pFile[i]) != NULL)
 						{
+							printf("C2: %s",buffer);
+							getchar();
 							if (getEventCode(buffer) == FENCE)
 							{
 								clockStr = (char *) malloc((size * 2 + 2) * sizeof(char));
@@ -136,28 +354,71 @@ int main(int argc, char **argv)
 	
 				for (i = index; i < size; i++)
                         	{
-                                	if (fgets(buffer, BUFFER_SIZE, pFile[i]) != NULL)
-                                	{
-						eventCode = getEventCode(buffer);
-                                        	if (eventCode != FENCE)
-						{
-							if (eventCode >= PUT && eventCode <= ACCUMULATE)
+					Chai aChain;
+					aChain.head = NULL;
+					aChain.tail = NULL;
+					while (true)
+					{
+                                		if (fgets(buffer, BUFFER_SIZE, pFile[i]) != NULL)
+                                		{
+							printf("C3: %s", buffer);
+							getchar();
+							eventCode = getEventCode(buffer);
+                                        		if (eventCode != FENCE)
 							{
-								//head[i]->next
+								if (eventCode >= PUT && eventCode <= ACCUMULATE)
+								{
+									tmpBuffer = buffer;
+									tmpStr = getData(&tmpBuffer);
+									free(tmpStr);
+									char *origin_addr = getData(&tmpBuffer);
+									tmpStr = getData(&tmpBuffer);
+									int target_rank = atoi(tmpStr);
+									free(tmpStr);
+									aList[i] = insertCommNode(aList[i], eventCode, origin_addr, 
+													target_rank, aList[target_rank].base);
+									
+									aChain = insertChainNode(aChain, eventCode, origin_addr);
+								}
+								else if (eventCode >= LOAD && eventCode <= STORE)
+								{
+									tmpBuffer = buffer;
+									tmpStr = getData(&tmpBuffer);
+									free(tmpStr);
+									char *varAddr;
+									varAddr = getData(&tmpBuffer);
+									aList[i] = insertLocaNode(aList[i], eventCode, varAddr);
+
+									aChain = insertChainNode(aChain, eventCode, varAddr);
+								}
+								else 
+								{ 
+									/* do nothing */ 
+								}
+							}
+							else //if (eventCode == FENCE)
+							{
+								break;
 							}
 						}
-						else
-						{
-							break;
-						}
                                 	}
+					
+					/* detect MCE within an epoch */
+					detectMCEInProc(aChain);
+
                         	}
+				
+				//dectect MCE accross processes
+
 			}
 			
-			else if (getEventCode(buffer) == 2)
+			else if (getEventCode(buffer) == BARRIER)
 			{
 			}
-			else { /*do nothing*/ }
+			else 
+			{ 
+				/* do nothing */ 
+			}
 		}
 		else 
 		{
@@ -170,7 +431,10 @@ int main(int argc, char **argv)
 		fclose(pFile[i]);
 	}
 	free(pFile);
-	free(head);
+
+	//End Of File
+
+	free(aList);
 
 	return 0;
 }
--- detector.h
+++ detector.h
@@ -6,51 +6,71 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define	BUFFER_SIZE	30
+#define	BUFFER_SIZE		30
 
-#define	FENCE		1
-#define	BARRIER		2
-#define	PUT		3
-#define	GET		4
-#define	ACCUMULATE	5
-#define	POST		6
-#define	START		7
-#define	COMPLETE	8
-#define	WAIT		9
-#define	LOCK		10
-#define	UNLOCK		11
-#define	SEND		12
-#define	RECV		13
-#define	LOAD		14
-#define	STORE		15
-#define CREATE		16
+#define	FENCE			1
+#define	BARRIER			2
+#define	GET			3
+#define	PUT			4
+#define	ACCUMULATE		5
+#define	POST			6
+#define	START			7
+#define	COMPLETE		8
+#define	WAIT			9
+#define	LOCK			10
+#define	UNLOCK			11
+#define	SEND			12
+#define	RECV			13
+#define	LOAD			14
+#define	STORE			15
+#define CREATE			16
 
-typedef struct {
-	char base[11];
-	int size;
-	int disp_unit;
-	void *next;
-} List;
-
-typedef struct {
-        int code;
-        void *next;
-} Comm;
-
-typedef struct {
+/*typedef struct {
         int code;
         int *clock;
         void *next;
-} Sync;
+} Sync;*/
 
-typedef struct {
+typedef struct Comm {
         int code;
-        void *varAddr;
-        void *next;
+	char *origin_addr;
+	int target_rank;
+	char *target_addr;
+	//int *clock;
+        struct Comm *next;
+} Comm;
+
+typedef struct Loca {
+        int code;
+        char *varAddr;
+	//int *clock;
+        struct Loca *next;
 } Loca;
 
+typedef struct List {
+        char *base;
+        int size;
+        int disp_unit;
+        struct Comm *commHead;
+	struct Comm *commTail;
+        struct Loca *locaHead;
+	struct Loca *locaTail;
+} List;
+
+typedef struct Chai {
+	struct Loca *head;
+	struct Loca *tail;
+} Chai;
+
+List insertCommNode(List, int, char *, int, char * /*, int *, int*/);
+List insertLocaNode(List, int, char *);
+Chai insertChain(Chai, int, char *);
+
 int getEventCode(char *);
+char *convertCode2Name(int);
 int *getClock(char *);
 bool equalClock(int *, int *, int);
+
+void detectMCEInProc(Chai);
 
 #endif
